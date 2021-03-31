d3b41fe887e536a1d1ca2a70740a05c50340c2e8
Achu2234 <75154223+Achu2234@users.noreply.github.com>
2021-03-31 15:47:23 +0530

    Update index.php

--- Bot/index.php
+++ Bot/index.php
@@ -75,8 +75,8 @@ Also add me to your group and make me admin. I'll reply to every photo😌.***",
 'parse_mode'=>"MarkDown",
 'reply_markup' =>  json_encode([
 'inline_keyboard' => [
-[['text' => "SUPPORT",'url' => "https://telegram.me/induschats"],['text' => "CREATOR", 'url' => "https://telegram.me/benchamxd"]],
-[['text' => "ADD ME", 'url' => "https://telegram.me/$BOT_USERNAME?startgroup=False"],['text' => "Repo", 'url' => "https://github.com/Benchamxd/Ai-Bot"]], 
+[['text' => "SUPPORT",'url' => "https://t.me/Yeageristbots"],['text' => "CREATOR", 'url' => "https://t.me/Amalbiju154"]],
+[['text' => "ADD ME", 'url' => "https://telegram.me/$BOT_USERNAME?startgroup=False"],['text' => "Our Bot Updates", 'url' => "https://t.me/Animemusicarchive6"]], 
 ]])
 ]);
 }
